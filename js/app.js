8334bda071eafde31f20c972537115f36900ffa8
travis <t3patterson@gmail.com>
2018-05-14 18:26:13 -0500

    creating new activity

---
+++ js/app.js
@@ -0,0 +1,38 @@
+//--------------------------------------------------------
+//TASK 1: Add an event listener to the <button class="rte-italics">
+//
+//        When user clicks, it should toggle the fontStyle property
+//        value of the <p class="textbox"> to 'italic'
+//
+//--------------------------------------------------------
+
+
+
+
+//--------------------------------------------------------
+//TASK 2: Add an event listener to the <button class="rte-bold">
+//
+//        When user clicks, it should toggle the fontWeight property
+//        value of the <p class="textbox"> to 700
+//
+//--------------------------------------------------------
+
+
+
+
+//--------------------------------------------------------
+//TASK 3: Add event listeners to the <button class="rte-fontsize"> elements
+//
+//        When user clicks, it should change the fontSize property of the
+//        <p class="textbox"> to the value of the button that was clicked
+//
+//--------------------------------------------------------
+
+
+//--------------------------------------------------------
+//TASK 4: Add event listeners to the <button class="rte-fontcolor"> elements
+//
+//        When user clicks, it should take the value from the 'data-color'
+//        attribute and set the 'color' property of the <p class="textbox">
+//
+//--------------------------------------------------------
